58e7d1cdff0b48129a46347c25e5352b15fd533e
spirittq <michejev@gmail.com>
2021-01-20 19:19:59 +0200

    Split code into functions

--- main.py
+++ main.py
@@ -8,6 +8,27 @@ logging.basicConfig(handlers=[logging.FileHandler(filename="errors.log",
                                                   encoding="utf-8")],
                     level=logging.INFO)
 
+
+def export_from(export_file):
+    with open(export_file, mode='r') as csv_file:
+        csv_reader = csv.DictReader(csv_file)
+        for row in csv_reader:
+            if row["CandidateOrCommittee"]:
+                add_dictionary_to_list(row)
+
+
+def import_to(new_csv, import_file):
+    with open(import_file, mode='w') as csv_file:
+        fieldnames = ["ReportFiledDate", "CandidateName", "PeriodBegining", "PeriodEnding", "TransactionID", "TransactionType", "TransactionAmount"]
+        writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+        writer.writeheader()
+        for row_dict in new_csv:
+            try:
+                writer.writerow(row_dict)
+            except Exception:
+                logging.info(row_dict)
+
+
 def add_dictionary_to_list(row):
     try:
         new_dict = {
@@ -23,12 +44,13 @@ def add_dictionary_to_list(row):
     except Exception:
         logging.info(row)
 
+
 def unix_format(date):
     date = date[:-3]
     return time.mktime(datetime.datetime.strptime(date, "%Y-%m-%d %H:%M:%S.%f").timetuple())
 
+
 def iso_format(date):
-    # print(date)
     new_date = datetime.datetime.strptime(date, "%m/%d/%Y")
     return datetime.datetime.strftime(new_date, "%Y-%m-%d")
 
@@ -36,33 +58,11 @@ def iso_format(date):
 if __name__ == "__main__":
 
     new_csv = []
+    export_file = 'transactions.csv'
+    import_file = 'result.csv'
     print("Starting export")
-    with open('transactions.csv', mode='r') as csv_file:
-        csv_reader = csv.DictReader(csv_file)
-        line_count = 1
-        for row in csv_reader:
-            line_count += 1
-            if row["CandidateOrCommittee"]:
-                add_dictionary_to_list(row)
-                # print(line_count)
-                # new_dict = {
-                #     "ReportFiledDate": unix_format(row["ReportFiledDate"]),
-                #     "CandidateName": row["CandidateFirstName"] + " " + row["CandidateLastName"],
-                #     "PeriodBegining": iso_format(row["PeriodBegining"]),
-                #     "PeriodEnding": iso_format(row["PeriodEnding"]),
-                #     "TransactionID": row["TransactionID"],
-                #     "TransactionType": row["TransactionType"],
-                #     "TransactionAmount": row["TransactionAmount"]
-                # }
-                # new_csv.append(new_dict)
+    export_from(export_file)
     print("Ending export")
     print("Starting import")
-
-    with open('result.csv', mode='w') as csv_file:
-        fieldnames = ["ReportFiledDate", "CandidateName", "PeriodBegining", "PeriodEnding", "TransactionID", "TransactionType", "TransactionAmount"]
-        writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
-        writer.writeheader()
-        for row_dict in new_csv:
-            writer.writerow(row_dict)
-
+    import_to(new_csv, import_file)
     print("Ending import")
